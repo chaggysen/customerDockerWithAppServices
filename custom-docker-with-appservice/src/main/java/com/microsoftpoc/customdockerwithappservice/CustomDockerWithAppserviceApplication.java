328a80e8405e9752832b3ae89f48b8983a6bfc4c
Sen Wang <sen.wang@mail.mcgill.ca>
2022-07-25 13:22:45 -0400

    boilerplate springboot application and dockerfile

---
+++ custom-docker-with-appservice/src/main/java/com/microsoftpoc/customdockerwithappservice/CustomDockerWithAppserviceApplication.java
@@ -0,0 +1,22 @@
+package com.microsoftpoc.customdockerwithappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CustomDockerWithAppserviceApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "welcome to custom docker with app service application";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(CustomDockerWithAppserviceApplication.class, args);
+	}
+
+}
---
+++ custom-docker-with-appservice/src/test/java/com/microsoftpoc/customdockerwithappservice/CustomDockerWithAppserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoftpoc.customdockerwithappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomDockerWithAppserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
